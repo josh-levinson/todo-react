1ace71e6dc1d75ebf20cdb1036af6c2018ac323a
Josh Levinson <joshua.levinson@gmail.com>
2023-10-01 10:27:57 -0400

    start basic data stuff

---
+++ src/data/create.js
@@ -0,0 +1,30 @@
+const {MongoClient} = require('mongodb');
+var config = require('../config');
+
+async function main() {
+  const uri = config.mongoUri;
+  const client = new MongoClient(uri);
+
+  try {
+    await client.connect();
+
+    await createTodo(client, {
+      description: "Test Todo",
+    });
+  } finally {
+    await client.close();
+  }
+}
+
+main().catch(console.error);
+
+/**
+ * Create a new todo
+ * @param {MongoClient} client A MongoClient that is connected to a cluster with the todo collection
+ * @param {Object} newTodo The new todo to be added
+ */
+
+async function createTodo(client, newTodo) {
+  const result = await client.db("todo_list").collection("todos").insertOne(newTodo);
+  console.log(`New todo created with id ${result.insertedId}`);
+}
